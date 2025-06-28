d2b48e4772fa959c6402a4b3e419a720820c8567
Fahim Sahriar <fahimsahriar.pg@gmail.com>
2025-06-28 17:17:54 +0600

    initial commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using DotNetMasteryProject.Data;
+using DotNetMasteryProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetMasteryProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            Console.WriteLine(categories);
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetMasteryProject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Category21Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
