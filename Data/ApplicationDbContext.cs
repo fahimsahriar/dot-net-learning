3a9bb6dd8b216913a2a035356f22da54dbed1c48
Fahim Sahriar <fahimsahriar.pg@gmail.com>
2025-06-30 15:26:13 +0600

    syncing remote repo with the downloaded repo version-part 2

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,25 +8,5 @@ namespace DotNetMasteryProject.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
-
-<<<<<<< HEAD
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Category>().HasData(
-        //        new Category { Category21Id = 1, Name = "Action", DisplayOrder = 1 },
-        //        new Category { Category21Id = 2, Name = "Gadget", DisplayOrder = 2 },
-        //        new Category { Category21Id = 3, Name = "Clothing", DisplayOrder = 3 }
-        //        );
-        //}
-=======
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Category>().HasData(
-                new Category { Category21Id = 1, Name = "Action", DisplayOrder = 1 },
-                new Category { Category21Id = 2, Name = "Gadget", DisplayOrder = 2 },
-                new Category { Category21Id = 3, Name = "Clothing", DisplayOrder = 3 }
-                );
-        }
->>>>>>> 9c34fb2ba4548b23c03c212b8dcb78b998505cdf
     }
 }
