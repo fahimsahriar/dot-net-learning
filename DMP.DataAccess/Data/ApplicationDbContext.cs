1c2d1745465772cc95790b37f022c1a22a33d671
Fahim Sahriar <shishirluth@gmail.com>
2025-07-06 14:11:54 +0600

    seperating the project in four different project

---
+++ DMP.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DMP.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace DMP.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Category21Id =1, Name = "Seattle", DisplayOrder = 1 },
+            new Category { Category21Id = 2, Name = "Vancouver", DisplayOrder = 2 },
+            new Category { Category21Id = 3, Name = "Mexico City", DisplayOrder = 3 },
+            new Category { Category21Id = 4, Name = "Puebla", DisplayOrder = 3 });
+        }
+    }
+}
---
+++ DMP.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DMP.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Category21Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="Display order can not be negative and greater than 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
